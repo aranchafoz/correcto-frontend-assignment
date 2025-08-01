0c99dac1f6d5a159216a7c7213f146b83a87a288
Arancha Ferrero <afe@caravelo.com>
2025-08-01 10:06:49 +0200

    chore: set body background color to avoid white flash

--- src/GlobalStyles.styles.ts
+++ src/GlobalStyles.styles.ts
@@ -1,13 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.colors.surfaceColor};
+  }
+
   #root {
     max-width: 100%;
     margin: 0 auto;
     padding: 2rem;
     box-sizing: border-box;
     text-align: center;
-    background-color: ${({ theme }) => theme.colors.surfaceColor};
   }
 
   h1 {
